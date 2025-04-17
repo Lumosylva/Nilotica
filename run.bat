f5ca0b6254c537030d37b6627590c72e95901086
Donny <donnymoving@gmail.com>
2025-04-17 09:15:33 +0800

    修改一键启动脚本run.bat，等待行情和订单执行网关启动完成

--- run.bat
+++ run.bat
@@ -7,6 +7,8 @@ set current_path=%~dp0
 echo Current path: %current_path%
 start %current_path%/bat/1_run_market_gateway.bat
 start %current_path%/bat/2_run_order_gateway.bat
+REM 等待行情网关和订单执行网关完全启动后再启动其他服务
+ping 127.0.0.1 -n 10 >nul
 start %current_path%/bat/3_run_strategy_subscriber.bat
 start %current_path%/bat/4_run_risk_manager.bat
 start %current_path%/bat/5_run_data_recorder.bat
