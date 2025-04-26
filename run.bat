d11894ba6f0cf87c324dee7b9562238a21b21f30
Donny <donnymoving@gmail.com>
2025-04-26 09:14:00 +0800

    Increase the delay time for other services to start

--- run.bat
+++ run.bat
@@ -1,5 +1,6 @@
 @echo off
 title Start all services
+REM Windows一键启动所有服务脚本
 :: activate Python
 call .venv\Scripts\activate
 echo Start all services......
@@ -8,7 +9,7 @@ echo Current path: %current_path%
 start %current_path%/bat/1_run_market_gateway.bat
 start %current_path%/bat/2_run_order_gateway.bat
 REM 等待行情网关和订单执行网关完全启动后再启动其他服务
-ping 127.0.0.1 -n 12 >nul
+ping 127.0.0.1 -n 14 >nul
 start %current_path%/bat/3_run_strategy_subscriber.bat
 start %current_path%/bat/4_run_risk_manager.bat
 start %current_path%/bat/5_run_data_recorder.bat
