ae932332546a4cd5c8373bc5e1a1f4668fe617cb
Donny <donnymoving@gmail.com>
2025-04-29 11:25:10 +0800

    Move the structlog module to the utils module

---
+++ utils/logger/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+@ProjectName: Nilotica_dev
+@FileName   : __init__.py
+@Date       : 2025/4/28 20:28
+@Author     : Donny
+@Email      : donnymoving@gmail.com
+@Software   : PyCharm
+@Description: description
+"""
---
+++ zmq_services/log_print_test.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+@ProjectName: Nilotica_dev
+@FileName   : log_print_test
+@Date       : 2025/4/28 17:41
+@Author     : Donny
+@Email      : donnymoving@gmail.com
+@Software   : PyCharm
+@Description: description
+"""
