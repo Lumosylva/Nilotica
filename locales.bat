fd61d8edab0bfbcba98189682b02bedb482326ac
Donny <donnymoving@gmail.com>
2025-05-11 23:20:35 +0800

    Add internationalization script

---
+++ locales.bat
@@ -0,0 +1,11 @@
+@echo off
+title Internationalization scripts
+:: Activate Python environment
+call .venv\Scripts\activate
+echo %CD%
+:: Generate pot file
+pybabel extract -F babel.cfg -o locales/messages.pot .
+:: Update an existing .po file with a new .pot file
+pybabel init -i locales/messages.pot -d locales -l en
+pybabel init -i locales/messages.pot -d locales -l zh_CN
+exit
