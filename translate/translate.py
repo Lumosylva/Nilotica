90e267b4c2e96a4fedeaa40ba2336895b0bd11f9
Donny <donnymoving@gmail.com>
2025-05-18 00:09:25 +0800

    Add Google Translate module and functionality

--- translate/translate.py
+++ translate/translate.py
@@ -13,13 +13,10 @@ import json
 import time
 import re
 import undetected_chromedriver as uc
-from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.common.exceptions import TimeoutException, NoSuchElementException
-from selenium.webdriver.common.action_chains import ActionChains
-
+from selenium.common.exceptions import TimeoutException
 import requests
 
 
